b5797299ca6ef151c08d8966233793db27823398
S. Chris Colbert <sccolbert@gmail.com>
2015-08-15 20:58:30 -0400

    move reference paths to separate _references file

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@
 |
 | The full license is in the file LICENSE, distributed with this software.
 |----------------------------------------------------------------------------*/
-/// <reference path="../typings/es6-containers/es6-containers.d.ts" />
 'use strict';
 
 
