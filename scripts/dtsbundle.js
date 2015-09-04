1d6fee9a31e7deb8f2cb5278fd889518aa4cf3da
S. Chris Colbert <sccolbert@gmail.com>
2015-09-04 00:21:43 -0400

    update to ts 1.6

--- scripts/dtsbundle.js
+++
@@ -1,7 +0,0 @@
-require('dts-generator').generate({
-  name: 'phosphor-signaling',
-  main: 'phosphor-signaling/index',
-  baseDir: 'lib',
-  files: ['index.d.ts'],
-  out: 'lib/phosphor-signaling.d.ts',
-});
